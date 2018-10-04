17e7135d76bea3e64fd4dc7304ea10e6213fa8a9
Derick Rethans <github@derickrethans.nl>
2018-10-04 15:41:08 +0100

    Get rid of separate debian-setup script

--- build-debian-linux-toolchain.sh
+++ build-debian-linux-toolchain.sh
@@ -7,6 +7,19 @@ set -o nounset
 
 # First argument is the architecture (32bit, when empty: x86_64)
 ARCH=$1
+PKG_ARCH=""
+
+# Add 32bit architecture if needed
+if (test "${ARCH}" = "32bit"); then
+    sudo dpkg --add-architecture i386
+    PKG_ARCH=":i386"
+fi
+
+# Install required packages
+sudo apt-get update
+sudo apt-get install -y g++-multilib make autoconf gcc bison locate pkg-config
+# Install architecture dependent packages
+sudo apt-get install -y libxml2-dev${PKG_ARCH} libicu-dev${PKG_ARCH} libz-dev${PKG_ARCH} libssl1.1${PKG_ARCH} libssl1.0.2${PKG_ARCH} libxslt1-dev${PKG_ARCH} libsasl2-dev${PKG_ARCH}
 
 # Where we're installing everything.
 INSTALL_DIR=$(pwd)/php
