789f15ef9e2aa0b91970ff44b53116af4903610e
Derick Rethans <github@derickrethans.nl>
2018-10-05 10:35:11 +0100

    Add missing build script

--- build-rhel-linux-toolchain.sh
+++ build-rhel-linux-toolchain.sh
@@ -5,32 +5,6 @@ set -o pipefail
 set -o xtrace
 set -o nounset
 
-# First argument is the architecture (32bit, when empty: x86_64)
-ARCH=$1
-PKG_ARCH=""
-
-# Add 32bit architecture if needed
-if (test "${ARCH}" = "32bit"); then
-    sudo dpkg --add-architecture i386
-    PKG_ARCH=":i386"
-fi
-
-# Install required packages
-sudo apt-get update
-sudo apt-get install -y g++-multilib make autoconf gcc bison locate pkg-config
-
-# Install architecture dependent packages, but Ubuntu has different libssl versions
-sudo apt-get install -y libxml2-dev${PKG_ARCH} libicu-dev${PKG_ARCH} libz-dev${PKG_ARCH} libxslt1-dev${PKG_ARCH} libsasl2-dev${PKG_ARCH}
-
-VARIANT=`cat /etc/*-release | grep ^ID= | sed 's/ID=//'`
-if (test "${VARIANT}" = "ubuntu"); then
-	sudo apt-get install -y libssl1.0.0${PKG_ARCH}
-else
-	sudo apt-get install -y libssl1.1${PKG_ARCH}
-fi
-sudo apt-get install -y libssl-dev${PKG_ARCH}
-
-
 # Where we're installing everything.
 INSTALL_DIR=$(pwd)/php
 
@@ -57,11 +31,7 @@ elif [[ $OPENSSL_MINOR_VERSION = "0" && $OPENSSL_PATCH_VERSION = "2" ]]; then
     PHP_RELEASES=$PHP_RELEASES_FOR_STABLE_OPENSSL
 fi
 
-if (test "${ARCH}" = "32bit"); then
-    BITNESS=32bit
-else
-    BITNESS=64bit
-fi
+BITNESS=64bit
 
 for phprel in $PHP_RELEASES
 do
@@ -69,4 +39,4 @@ do
     PREFIX="$INSTALL_DIR" ./build-debian-single.sh "$phprel" debug zts ${BITNESS}
 done
 
-tar -czvf php.tar.gz ${INSTALL_DIR}
+tar -czf php.tar.gz php
